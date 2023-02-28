1b988939862985734cee32a2e2fda69507a31bd5
Calli Herrmann <calliherrmann@gmail.com>
2023-02-28 15:41:48 -0500

    Change string variables

--- variables.js
+++ variables.js
@@ -1,6 +1,6 @@
 var cat = 'Pepper';
-var name = 'Calli'
-var computer = 'Apple'
+var firstName = 'Calli';
+var computer = 'Apple';
 
 var age = 26
 var carYear = 2011
@@ -9,3 +9,11 @@ var currentYear = 2023
 var hungry = false
 var tired = false
 var energized = true
+
+cat = 'Maisy';
+firstName = 'Hillery';
+computer = 'Banana';
+
+console.log(cat);
+console.log(firstName);
+console.log(computer);
